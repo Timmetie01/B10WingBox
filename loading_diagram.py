fad50e2bd8cc34b5b6fa8446b126fc84c6057e00
CelerynCZ <adam@celeryn.cz>
2025-11-18 17:09:21 +0100

    Add ISA functionality

--- loading_diagram.py
+++ loading_diagram.py
@@ -1,1 +1,28 @@
 #We will try to make the loading diagram here, hopefully should be fun!!!!!
+
+def ISA(alt):
+    g_0 = 9.80665
+    R = 287
+    T_0 = 288.15
+    p_0 = 101325
+    a = -0.0065
+
+    alt1 = min(11000, alt)
+
+    t_1 = T_0 + a*(alt1)
+
+    p_1 = p_0*((t_1/T_0)**((-g_0)/(a*R)))
+
+    rho = p_1/(t_1*R)
+
+    if alt <= 11000:
+        return t_1, p_1, rho
+    
+    alt1 = min(20000,alt)
+
+    p_1 = p_1*math.exp((-g_0/(R*t_1))*(alt1-11000))
+
+    rho = p_1/(t_1*R)
+
+    return t_1, p_1, rho
+
