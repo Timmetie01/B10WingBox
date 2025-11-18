21776e5b486647d0f677e62aa21b87f5778f631a
clopezdiaztu <C.LopezDiaz@student.tudelft.nl>
2025-11-18 18:03:45 +0100

    Updated organised array of coefficients

--- Chapter4.1/xflr5org.py
+++ Chapter4.1/xflr5org.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-tablezeroalpha = np.genfromtxt("B10WingBox\Chapter4.1\MainWing_a=0.00HighpanelFinal.csv", delimiter=",", skip_header=21, skip_footer=5745)
+tablezeroalpha = np.genfromtxt("B10WingBox\Chapter4.1\MainWing_a=0.00HighpanelFinal.csv", delimiter=",", skip_header=91, skip_footer=5745)
 
 yspan_0 = tablezeroalpha[:,0]
 chordlength_0 = tablezeroalpha[:,1]
@@ -12,7 +12,7 @@ cmgeom_0 = tablezeroalpha[:,6]
 CmAirfchord4_0 = tablezeroalpha[:,7]
 posofcp_0 = tablezeroalpha[:,10]
 
-tabletenalpha = np.genfromtxt("B10WingBox\Chapter4.1\MainWing_a=10.00HighplaneFinal.csv", delimiter=",", skip_header=21, skip_footer=5745)
+tabletenalpha = np.genfromtxt("B10WingBox\Chapter4.1\MainWing_a=10.00HighplaneFinal.csv", delimiter=",", skip_header=91, skip_footer=5745)
 
 yspan_10 = tabletenalpha[:,0]
 chordlength_10 = tabletenalpha[:,1]
