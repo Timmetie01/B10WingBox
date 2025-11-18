db48e37e9743bf2212a2a7816599bb484d34125f
Timmetie01 <tnmoosterom@hotmail.com>
2025-11-18 17:07:13 +0100

    Fixing constants file (hopefully)

--- constants.py
+++ constants.py
@@ -1,64 +1,59 @@
-import math as m
+import math
+
 
 const = {
-    #General specs
-    'mtom': 12314.28,
-    'MTOM': 12314.28,
-    'eom': 6763.18,
-    'EOM': 6763.18,
-    'TW': 0.4779,
+    # General Specs
+    'maximum_take_off_mass': 12314.28, # [kg]
+    'empty_operating_mass': 6763.18, # [kg]
+    'thrust_to_weight_ratio': 0.4779, # [-]
 
-    #Planform specs
-    'span': 17.59150919,
-    'AR': 8.6,
-    'Sw': 35.98385994,
-    'wing_area': 35.98385994,
-    'le_sweep': 26.8227774 * m.pi / 180,
-    'cr': 3.108170068,
-    'root_chord': 3.108170068,
-    'ct': 0.98287858,
-    'tip_chord': 0.98287858,
-    'taper': 0.316224196,
-    'taper_ratio': 0.316224196,
-    'MAC': 2.229538428,
-    'mean_aerodynamic_chord': 2.229538428,
-    'Cl_cruise': 0.580201347,
-    'Cl_Cd_cruise': 13.305,
-    'Cl_alpha': 5.537,
-    'flap_start_pos': 1.050,
-    'flap_end_pos': 4.423,
+    # Planform Specs
+    'span': 17.59150919, # [-]
+    'aspect_ratio': 8.6, # [-]
+    'wing_area': 35.98385994, # [m^2]
+    'leading_edge_sweep': 26.8227774 * math.pi / 180, # [rad]
+    'root_chord': 3.108170068, # [m]
+    'tip_chord': 0.98287858, # [m]
+    'taper_ratio': 0.316224196, # [-]
+    'mean_aerodynamic_chord': 2.229538428, # [m]
+    'lift_coefficient_cruise': 0.580201347, # [-]
+    'lift_to_drag': 13.305, # [-]
+    'lift_coefficient_slope': 5.537, # [1/rad]
+    'flap_start_position': 1.050, # [m]
+    'flap_end_position': 4.423, # [m]
 
     #Fuselage specs
-    'fus_length': 15.85,
-    'nose_length': 3,
-    'tailcone_length': 6.234,
-    'fus_diameter': 2.078,
+    'fuselage_length': 15.85, # [m]
+    'nose_length': 3, # [m]
+    'tailcone_length': 6.234, # [m]
+    'fuselage_diameter': 2.078,# [m]
 
     #Landing Gear specs
-    'ground_to_fuselage_height':0.9795,
-    'main_lg_wheel_diameter': 0.5588,
-    'main_lg_width': 0.17145,
-    'nose_lg_diameter': 0.5588,
-    'nose_lg_width': 0.17145, 
+    'ground_to_fuselage_height': 0.9795, # [m]
+    'main_landing_gear_wheel_diameter': 0.5588, # [m]
+    'main_landing_gear_width': 0.17145, # [m]
+    'nose_landing_gear_diameter': 0.5588, # [m]
+    'nose_landing_gear_width': 0.17145, # [m]
 }
 
 
-#sweep angle (IN RADIANS) at fraction of chord. 0.25 => quarter chord
-#input x = fraction of chord
-#output is integer of sweep (in radians!!)
-def sweep(x, le_sweep=const['le_sweep'], span=const['span'], taper=const['taper'], cr=const['cr']):
-    if x < 0 or x > 1:
-        print(f'Can not compute sweep. x={x}, it should be 0 =< x =< 1.')
+def sweep_at_chord_fraction(chord_fraction, leading_edge_sweep = const['leading_edge_sweep'], span = const['span'], taper = const['taper_ratio'], root_chord = const['root_chord']):
+    if  chord_fraction < 0 or chord_fraction > 1:
+        print(f'Cannot compute sweep. Chord fraction = {chord_fraction}, should be 0 =< chord_fraction =< 1.')
         quit()
     
-    #Formula obtained from ADSEE project
-    return m.atan(m.tan(le_sweep) - x * 2 * cr / span * (1-taper))
+    # ADSEE formula for sweep at chord fraction
+    chord_fraction_sweep = math.atan(math.tan(leading_edge_sweep) - chord_fraction * 2 * root_chord / span * (1-taper)) # [rad]
 
-#calculate the chord length at a certain span location y
-def chord(y, cr=const['cr'], taper=const['taper'], span=const['span']):
-    if abs(y) > span:
-        print(f'entering a location of y={y}, but its absolute value can only be as large as the span of b={span}.')
+    return chord_fraction_sweep
+
+
+def local_chord_at_span(target_span, root_chord = const['root_chord'], taper_ratio = const['taper_ratio'], total_span = const['span']):
+    if abs(target_span) > total_span:
+        print(f'Target span out of bounds')
         quit()
 
-    #Just the standard formula for a trapezedoidal wing
-    return cr - cr * (1-taper) * abs(y) / (span / 2)
+    # ADSEE formula for trapezoidal planform
+    local_chord = root_chord - root_chord * (1-taper_ratio) * abs(target_span) / total_span / 2
+
+    return local_chord
