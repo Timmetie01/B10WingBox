30a22539aff56d797e6d3bd0732c3ebc10cb6011
Timmetie01 <tnmoosterom@hotmail.com>
2025-11-20 14:46:26 +0100

    Started setting up classes, fixes to panel reader

---
+++ classes.py
@@ -0,0 +1,6 @@
+class Wingbox:
+    def __init__(self, wingboxpanels, panel_thickness, stringercoords, stringer_area):
+        self.panels = wingboxpanels
+        self.panel_thickness = panel_thickness
+        self.stringers = stringercoords
+        self.stringerarea = stringer_area
