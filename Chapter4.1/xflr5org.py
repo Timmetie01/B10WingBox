6470c716b6acba16c32eb155b459262f62c78a9a
JanPetr287 <jan.petr287@gmail.com>
2025-11-20 15:51:17 +0100

    Added langing gear weight distribution function and a CLd to alpha function

--- Chapter4.1/xflr5org.py
+++ Chapter4.1/xflr5org.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy as sp
 from scipy import interpolate
 import matplotlib.pyplot as plt
+import math as m
 
 #Constants
 cr = 3.108170068
@@ -12,6 +13,7 @@ rho = 0.28711101
 vinfinity = 200.5593111
 q = 0.5*rho*vinfinity**2
 CLcruise = 0.58020
+g = 9.80665
 
 #From xflr5
 CL0 =  0.179632
@@ -19,7 +21,7 @@ CL10 = 1.004670
 
 
 #Organised aerodynamic coefficients
-tablezeroalpha = np.genfromtxt("B10WingBox\Chapter4.1\MainWing_a=0.00HighpanelFinal.csv", delimiter=",", skip_header=91, skip_footer=5743)
+tablezeroalpha = np.genfromtxt("Chapter4.1\\MainWing_a=0.00HighpanelFinal.csv", delimiter=",", skip_header=91, skip_footer=5743)
 
 yspan_0 = tablezeroalpha[:,0]
 chordlength_0 = tablezeroalpha[:,1]
@@ -32,7 +34,7 @@ CmAirfchord4_0 = tablezeroalpha[:,7]
 posofcp_0 = tablezeroalpha[:,10]
 
 
-tabletenalpha = np.genfromtxt("B10WingBox\Chapter4.1\MainWing_a=10.00HighplaneFinal.csv", delimiter=",", skip_header=91, skip_footer=5743)
+tabletenalpha = np.genfromtxt("Chapter4.1\\MainWing_a=10.00HighplaneFinal.csv", delimiter=",", skip_header=91, skip_footer=5743)
 
 yspan_10 = tabletenalpha[:,0]
 chordlength_10 = tabletenalpha[:,1]
@@ -73,6 +75,57 @@ def chordlength2(cr,taper,span,ypos):
     chordlength2 = cr -cr*(1-taper)*ypos/(span/2)
     return chordlength2
 
+def weightdistribution(y: float, alpha: float, step: float = span/2 / 200, LG_weight: float = 317.833/2, LG_y_pos: float = 1.9808):
+    """A function that represents the weight distribution of the landing gear
+    
+    Parameters
+    ----------
+    y: float
+        y position for which the weight should be given
+    alpha: float
+        angle of attack
+    step:
+        the step size of the integration to create a range in which the point mass acts
+        Default is span/400
+    LG_weight: float
+        mass of one side of main LG in [kg]
+        Default is 317.833/2
+    LG_y_pos: float
+        y position of the main landing gear CG
+        Default is 1.9808
+    
+    Returns
+    -------
+    weight_at_y: float
+        weight at y position in [N]
+    """
+    
+    weight_at_y = 0 #add weight at chord, fuel at chord...
+    
+    if y - step/2 < LG_y_pos and LG_y_pos > y + step/2:
+        weight_at_y += LG_weight * g
+
+    return weight_at_y * m.cos(alpha)
+
+def alphafromCLd(CLd):
+    """A function that gives the alpha for a given CLd. Assumes that the lift coefficient is linear between 0 and 10 degrees.
+    
+    Parameters
+    ----------
+    CLd: float
+        the desired lift coefficient
+    
+    Returns
+    -------
+    alpha: float
+        angle of attack [rad]
+
+    """
+
+    alpha = m.pi/18 * (CLd - CL0)/(CL10-CL0)
+    return alpha
+
+
 #Aerodynamic distribution for known CLd (it will give a function dependent on spanwise position)
 
 def liftdristribution(CLd):
@@ -85,12 +138,18 @@ def liftdristribution(CLd):
 def Lub(y):
     return liftdristribution(CLcruise)(y)*q*chordlength(y,cr,ct,span)
 
-def shear(y):
+def shear(y, alpha):
     S, error = sp.integrate.quad(Lub,y,span/2)
-    return -1*S
+    W, error2 = sp.integrate.quad(weightdistribution, y, span/2, args = (alpha)) # add the weight distribution
+    return -1*(S-W)
+
+
 
 ypoints = np.linspace(0, span/2,200)
-shearvalues = np.array([shear(y) for y in ypoints])
+shearvalues = np.array([shear(y, 10*m.pi/180) for y in ypoints])
+
+print(shearvalues.min())
+
 
 plt.plot(ypoints,shearvalues)
 plt.xlabel("Spanwise position [m]")
