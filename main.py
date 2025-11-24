b2b3a2ab12a9b30de37ecfeb0da16195f8780d96
Timmetie01 <tnmoosterom@hotmail.com>
2025-11-24 09:46:26 +0100

    Bugfix

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ import area_moments
 import constants
 
 testclass = data_import.import_wingbox('test_cross_section')
-testscale = classes.ScaledWingbox(testclass, 2)
 
 #print(testclass.centroidal_panels)
 #print(testscale.centroidal_panels)
 
 #The Ixx, Iyy and Ixy functions must be checked to confirm if they work. 
 print(testclass.Ixx(0))
-print(testclass.Ixx(4))
-print(testclass.Ixx(8))
+print(testclass.Iyy(0))
+print(testclass.Ixy(0))
 
