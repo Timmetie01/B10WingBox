d7a399661f57f8c64044693aef78a214bc7d72b0
ellakoolen13 <ellakoolen13@gmail.com>
2025-11-25 14:55:53 +0100

    twist deflection function

--- deflection_functions.py
+++ deflection_functions.py
@@ -1,1 +1,28 @@
+import numpy as np
+import scipy as sp
+from constants import const
+from area_moments import Torsional_constant
+import data_import
+import classes
+
+testclass = data_import.import_wingbox('test_cross_section')
+
+
+def T(y):
+    b = const['span']
+    return 5000*(1-y)/(b/2)
+
+def dtheta_dz(y):
+    G = const['Shear_Modulus']
+    return T(y)/(G*testclass.J(y))
+def theta(y):
+    result, error = sp.integrate.quad(dtheta_dz,0,y)
+    return result
+
+
+
+
+
+
+
 
