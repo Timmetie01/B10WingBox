42373e404d69017906ad652162400b52edfa2235
Timmetie01 <tnmoosterom@hotmail.com>
2025-11-27 16:23:02 +0100

    test

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ testclass = data_import.import_wingbox('test_cross_section')
 
 graphing.airfoil_pointplot(showplot=False)
 
-newtestclass = data_import.create_airfoil_like_wingbox(0.1, 0.6, thickness=[0.003, 0.001, 0.003, 0.001], thicknesstype='full_array', stringercount=12, stringer_areas=2e-5, panelcount=4)
+newtestclass = data_import.create_airfoil_like_wingbox(0.1, 0.6, thickness=[0.003, 0.001, 0.003, 0.001], thicknesstype='full_array', stringercount=12, stringer_areas=2e-5, panelcount=50)
 
 graphing.wingbox_plot(newtestclass)
 graphing.deflection_plot(newtestclass, two_wings=False)
