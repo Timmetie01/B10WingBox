d7f7c77a4dc946404cf04e48d624469981206f4f
Timmetie01 <tnmoosterom@hotmail.com>
2025-12-01 11:50:46 +0100

    Convenient iteration

--- main.py
+++ main.py
@@ -7,25 +7,14 @@ import numpy as np
 
 testclass = data_import.import_wingbox('test_cross_section')
 
-#Testing the I-plot
-#graphing.I_plot(testclass)
-
-#Testing the deflection plots, just 
-#from data_import import import_wingbox
-#testclass = import_wingbox('test_cross_section')
-#graphing.deflection_plot(testclass)
-#graphing.twist_plot(testclass)
-
-
-graphing.worst_moment_plot()
-graphing.worst_torsion_plot()
+#graphing.I_plot(iterationwingbox)
 
 deflection = 100
 twist = 100
 wingboxthickness = 0
 while deflection > const['span'] * const['max_deflection_fraction'] or abs(twist) > const['max_twist_degrees']:
-    wingboxthickness += 1e-4
-    iterationwingbox = data_import.create_airfoil_like_wingbox(0.1, 0.5, thickness=[wingboxthickness, wingboxthickness, wingboxthickness,  wingboxthickness], thicknesstype='partially_constant', stringercount=18, stringer_areas=2e-5, panelcount=50, stringerspacing='constant_no_endpoints')
+    wingboxthickness += 5e-5
+    iterationwingbox = data_import.create_airfoil_like_wingbox(0.1, 0.5, thickness=[wingboxthickness, wingboxthickness, wingboxthickness,  wingboxthickness], thicknesstype='partially_constant', stringercount=20, stringer_areas=1e-5, panelcount=50, stringerspacing='constant_no_endpoints')
 
     y_list, v_list = deflection_functions.v(iterationwingbox)
     deflection = max(v_list)
@@ -33,9 +22,9 @@ while deflection > const['span'] * const['max_deflection_fraction'] or abs(twist
     theta_list = theta_list * 180 / np.pi
     twist = max(np.abs(theta_list))
 
-    print(wingboxthickness, twist)
+    print(f'Thickness: {round(const['root_chord'] * wingboxthickness * 1000, 5)} mm  \ttwist: {round(twist, 4)} deg  \tdeflection: {round(deflection, 4)} m.')
 
-print(wingboxthickness)
+print(f'The final wingbox thickness is {round(const['root_chord'] * wingboxthickness * 1000, 5)} mm at the chord, and {round(const['tip_chord'] * wingboxthickness * 1000, 5)} mm at the tip!')
 
 
 graphing.airfoil_pointplot(showplot=False)
