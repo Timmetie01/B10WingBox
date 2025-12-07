08e67dd5e64f140e71c455c2e042bf8de32ce255
Timmetie01 <tnmoosterom@hotmail.com>
2025-12-07 12:07:14 +0100

    Release test

--- classes.py
+++ classes.py
@@ -26,18 +26,22 @@ class Wingbox:
         self.centroidal_panels = self.panels - np.array([self.centroid_coordinates[0], self.centroid_coordinates[1], self.centroid_coordinates[0], self.centroid_coordinates[1]])
         self.centroidal_stringers = self.stringers - self.centroid_coordinates
     
+    #Returns the Ixx at a certain spanwise location. Ixx is around chord-wise axis
     def Ixx(self, y):
         import area_moments
         return area_moments.second_area_moment(y, self)[0]
     
+    #Returns the Iyy at a certain spanwise location. Iyy is around vertical axis
     def Iyy(self, y):
         import area_moments
         return area_moments.second_area_moment(y, self)[1]
     
+    #Returns the Ixy at a certain spanwise location. Due to simplifications this can be assumed to be 0.
     def Ixy(self, y):
         import area_moments
         return area_moments.second_area_moment(y, self)[2]
     
+    #Returns the Torsional constant of the wingbox at a certain spanwise location/
     def J(self, y):
         import area_moments
         import constants
@@ -45,30 +49,37 @@ class Wingbox:
         current_wingbox = ScaledWingbox(self, scale)
         return area_moments.Torsional_constant(current_wingbox.centroidal_points, current_wingbox.panel_thickness)
     
+    #Shows the point highest and lowest compared to the neutral axis. Required for normal stress due to bending.
     def z_max_min(self, y):
         import area_moments
         return area_moments.z_max_min(y, self)
 
+    #Plots the wingbox: Panels and Stringers
     def plot(self, showplot=True):
         import graphing
         graphing.wingbox_plot(self, showplot)
 
+    #Plots a 3D representation of the wing under worst-case loading
     def wing_plot(self, Npoints=50, twowings=False):
         import graphing
         graphing.wing_plot(self, Npoints, twowings)
 
+    #Plots the deflection of the wing as a function of spanwise position. Show_wing plots the top and bottom surface of the wing too (Looks a lot better)
     def deflection_plot(self, show_wing=True, two_wings=False):
         import graphing
         graphing.deflection_plot(self, show_wing, two_wings)
 
+    #Plots the twist distribution along the span of the wing
     def twist_plot(self):
         import graphing
         graphing.twist_plot(self)
 
+    #Plots the Ixx, Iyy, Ixy and J at each spanwise position of the wing.
     def I_plot(self,npoints=100):
         import graphing
         graphing.I_plot(self, npoints)
     
+    #Prints the highest bending stress present along the entire wing, by iterating along its span and checking for the highest value
     def max_bending_stress(self, Npoints=250):
         import stress_functions
         import constants
@@ -88,13 +99,15 @@ class Wingbox:
         
         return max(stress_tab), y_max_stress
     
+    #Returns the area inside the panels of the wingbox (used for fuel volume calculations)
     def area(self, y):
         import area_moments
         import constants
         current_wingbox = ScaledWingbox(self, constants.local_chord_at_span(y))
         return area_moments.polygon_area(current_wingbox.points)
     
-    def weight(self):
+    #Returns (and optionally prints) the weight of the entire wingbox.
+    def weight(self, print_value=True):
         import area_moments
         y_tab = np.linspace(0, const['span']/2, 1000)
         area_tab = []
@@ -103,7 +116,8 @@ class Wingbox:
 
         volume = 2 * sp.integrate.cumulative_trapezoid(area_tab, y_tab, initial=0)[-1]
         mass = volume * const['Density']
-        print(f'The total wingbox weights {round(mass, 3)} kg.')
+        if print_value:
+            print(f'The total wingbox weights {round(mass, 3)} kg.')
         return mass
         
     
