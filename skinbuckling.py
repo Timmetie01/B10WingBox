f1ee18e1a60cc77e78027484cc0865eafe76c251
clopezdiaztu <C.LopezDiaz@student.tudelft.nl>
2025-12-09 16:56:12 +0100

    Skin buckling updated

--- skinbuckling.py
+++ skinbuckling.py
@@ -28,18 +28,21 @@ def get_panel_length(wingbox):
 toverb_list = []
 
 #change wingbox argument to specific wingbox
-for i in range(len(Wingbox.panels)):
-    toverbratio = get_panel_t(design3_wingbox)[i]/get_panel_length(design3_wingbox)[i]
+for i in range(len(design3_wingbox.panels)):
+    toverbratio = get_panel_t(design3_wingbox)[i][0]/get_panel_length(design3_wingbox)[i][0]
     toverb_list.append(toverbratio)
 
 panel_critical_stress_lst = []
 
-for i in range(len(Wingbox.panels)):
+for i in range(len(design3_wingbox.panels)):
     critical_stress = pi**2*k_c*E/(12*(1-v_poisson**2)) * toverb_list[i]**2
     panel_critical_stress_lst.append(critical_stress)
 
 total_critical_stress = sum(panel_critical_stress_lst)
 
+#print(total_critical_stress)
+#print(get_panel_length(design3_wingbox))
+print(panel_critical_stress_lst)
 
 #2. Generate approximated graph to calculate k_c out of a/b
 #In the end we assume that given that a is extremely large compared to panel length, it tends to infinity, in the graph it is around 7.1
