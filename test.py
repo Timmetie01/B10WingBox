3add99cbe1993ceab9cb061e81cd734358dbf1fc
Timmetie01 <tnmoosterom@hotmail.com>
2025-12-09 14:25:10 +0100

    Shear calculations

--- test.py
+++ test.py
@@ -7,7 +7,4 @@ from constants import const
 import area_moments
 import wingbox_design
 
-iterationwingbox, thickness = wingbox_design.thickness_iteration(0.2, 0.6, stringercount=20, stringer_areas=3e-5, stringerspacing='constant_no_endpoints', panelcount=50)
-
-area_moments.cross_sectional_area(iterationwingbox, 0)
-
+graphing.worst_shear_plot()
