c16dc6e935868db5db554167bd7052d580148cc2
Timmetie01 <tnmoosterom@hotmail.com>
2025-12-18 14:36:38 +0100

    Rounded wingboxes

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import graphing
 print( 0.0009140942779731522/0.0028076928753693084)
 
 #The final designs after iteration. First the constant thickness one:
-test_unscaled_wingbox = data_import.idealizable_wingbox(0.2, 0.6, [ 0.0028076928753693084, 0.0038648013451746885,  0.0028076928753693084, 0.0038648013451746885], 'partially_constant', 26, 3.624271793503936e-05, scaled_thickness=False)
-graphing.plot_MOS_graph(test_unscaled_wingbox)
+#test_unscaled_wingbox = data_import.idealizable_wingbox(0.2, 0.6, [ 0.0028076928753693084, 0.0038648013451746885,  0.0028076928753693084, 0.0038648013451746885], 'partially_constant', 26, 3.624271793503936e-05, scaled_thickness=False)
+#graphing.plot_MOS_graph(test_unscaled_wingbox)
 
 test_unscaled_rounded_wingbox = data_import.idealizable_wingbox(0.2, 0.6, [ 0.0029, 0.0039,  0.0029, 0.0039], 'partially_constant', 26, 3.7e-05, scaled_thickness=False)
 graphing.plot_MOS_graph(test_unscaled_rounded_wingbox)
@@ -13,10 +13,10 @@ graphing.plot_MOS_graph(test_unscaled_rounded_wingbox)
 
 
 #THe final design after iteration, here the variable thickness one
-test_scaled_wingbox = data_import.idealizable_wingbox(0.2, 0.6, [0.0009140942779731522, 0.001263453670307925, 0.0009140942779731522, 0.001263453670307925], 'partially_constant', 28, 3.0285835970395364e-05, scaled_thickness=True)
-graphing.plot_MOS_graph(test_scaled_wingbox)
+#test_scaled_wingbox = data_import.idealizable_wingbox(0.2, 0.6, [0.0009140942779731522, 0.001263453670307925, 0.0009140942779731522, 0.001263453670307925], 'partially_constant', 28, 3.0285835970395364e-05, scaled_thickness=True)
+#graphing.plot_MOS_graph(test_scaled_wingbox)
 
-test_scaled_rounded_wingbox = data_import.idealizable_wingbox(0.2, 0.6, [0.0010, 0.0013, 0.0010, 0.0013], 'partially_constant', 28, 3.1e-05, scaled_thickness=True)
+test_scaled_rounded_wingbox = data_import.idealizable_wingbox(0.2, 0.6, [0.0009, 0.0013, 0.0009, 0.0013], 'partially_constant', 28, 3.4e-05, scaled_thickness=True)
 graphing.plot_MOS_graph(test_scaled_rounded_wingbox)
 
 
