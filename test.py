c37a1c3da491aa12efb335f715a2c8eadd9b2cf9
Timmetie01 <tnmoosterom@hotmail.com>
2025-12-18 14:29:38 +0100

    rounded values

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ print( 0.0009140942779731522/0.0028076928753693084)
 test_unscaled_wingbox = data_import.idealizable_wingbox(0.2, 0.6, [ 0.0028076928753693084, 0.0038648013451746885,  0.0028076928753693084, 0.0038648013451746885], 'partially_constant', 26, 3.624271793503936e-05, scaled_thickness=False)
 graphing.plot_MOS_graph(test_unscaled_wingbox)
 
-#test_unscaled_rounded_wingbox = data_import.idealizable_wingbox(0.2, 0.6, [ 0.0019, 0.0035,  0.0019, 0.0035], 'partially_constant', 42, 1.03e-05, scaled_thickness=False)
-#graphing.plot_MOS_graph(test_unscaled_rounded_wingbox)
+test_unscaled_rounded_wingbox = data_import.idealizable_wingbox(0.2, 0.6, [ 0.0029, 0.0039,  0.0029, 0.0039], 'partially_constant', 26, 3.7e-05, scaled_thickness=False)
+graphing.plot_MOS_graph(test_unscaled_rounded_wingbox)
 
 
 
@@ -16,5 +16,7 @@ graphing.plot_MOS_graph(test_unscaled_wingbox)
 test_scaled_wingbox = data_import.idealizable_wingbox(0.2, 0.6, [0.0009140942779731522, 0.001263453670307925, 0.0009140942779731522, 0.001263453670307925], 'partially_constant', 28, 3.0285835970395364e-05, scaled_thickness=True)
 graphing.plot_MOS_graph(test_scaled_wingbox)
 
+test_scaled_rounded_wingbox = data_import.idealizable_wingbox(0.2, 0.6, [0.0010, 0.0013, 0.0010, 0.0013], 'partially_constant', 28, 3.1e-05, scaled_thickness=True)
+graphing.plot_MOS_graph(test_scaled_rounded_wingbox)
 
 
