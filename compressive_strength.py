6a4e5d1bc00babc8a3815ab8793ff63bfd383f9e
Timmetie01 <tnmoosterom@hotmail.com>
2025-12-18 13:37:27 +0100

    slight optimization

--- compressive_strength.py
+++ compressive_strength.py
@@ -8,28 +8,35 @@ from constants import local_chord_at_span
 from constants import const
 
 
-wingbox = import_wingbox('test_cross_section')
-y_span = np.linspace(0, const['span']/2, 200)
-sigma_tensile = np.zeros_like(y_span)
-sigma_compressive = np.zeros_like(y_span)
+#wingbox = import_wingbox('test_cross_section')
+
 
 critical_sigma_z_tensile=const['Yield_stress'] # Pa
 critical_sigma_z_compressive=-1 * const['Yield_stress'] # Pa
 
-data = np.loadtxt(r"data\test_cross_section\stringer_properties.txt", skiprows=1)
-z_positions = data[:, 1] 
+
 
 def bending_stress(wingbox, y):
     # Bending stress formula: sigma = M * z / Ixx
+    data = np.loadtxt(r"data\test_cross_section\stringer_properties.txt", skiprows=1)
+    z_positions = data[:, 1] 
+
     sigma_z = worst_case_loading.M(y, 'abs_min_bending') * z_positions *local_chord_at_span(y)/ wingbox.Ixx(y)
     return sigma_z
 
-for i, y in enumerate(y_span):
-    sigma_z = bending_stress(wingbox, y)
-    sigma_tensile[i] = np.max(sigma_z) 
-    sigma_compressive[i] = np.min(sigma_z) 
+
 
 def worst_bending_MOS(wingbox, Npoints=200):
+    y_span = np.linspace(0, const['span']/2, 200)
+    sigma_tensile = np.zeros_like(y_span)
+    sigma_compressive = np.zeros_like(y_span)
+
+    for i, y in enumerate(y_span):
+        sigma_z = bending_stress(wingbox, y)
+        sigma_tensile[i] = np.max(sigma_z) 
+        sigma_compressive[i] = np.min(sigma_z) 
+
+
     y_span = np.linspace(0, const['span']/2, Npoints)
     sigma_tensile = np.zeros_like(y_span)
     sigma_compressive = np.zeros_like(y_span)
